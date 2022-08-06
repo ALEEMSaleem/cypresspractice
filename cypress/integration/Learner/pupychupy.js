2075287f1c3cd711e7cad82d2c879749aec35800
muhammad saleem <sarowerhome@gmail.com>
2022-08-06 16:15:45 -0400

    first commit

---
+++ cypress/integration/Learner/pupychupy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+describe('practice', ( )=> {
+    it('practicing cypress', () =>{
+
+        cy.visit('https://automationteststore.com/');
+        cy.get('[class="active menu_home"]').click();
+
+    })
+
+
+
+
+
+
+
+
+
+})
---
+++ hiworld.js
@@ -0,0 +1,22 @@
+// let age:string = '25'
+// console.log(age)
+// let check:any = 'aleem'
+// console.log(check)
+// check = 28
+// console.log(check)
+// let empInfo: string|number|boolean ="My name Is Aleem"
+// //console.log(empInfo)
+// empInfo = 28
+// console.log(empInfo)
+// empInfo = true
+// console.log(empInfo)
+// let hours:string[]=["age","time","money"]
+// let hours:string[]=["age","time","money"]
+// let hours1 = hours.push('saleem')
+// console.log(hours)
+//let hours:string[]=["age","time","money"];
+// console.log(hours)
+// hours.pop();
+//  let hours2 = hours.indexOf('age')
+// //const hours:string="24"
+//console.log(hours[1])
