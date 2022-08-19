01bba7c108a2eaf69d239560888dc29da2e6662e
muhammad saleem <sarowerhome@gmail.com>
2022-08-18 22:39:58 -0400

    new

--- cypress/integration/Learner/pupychupy.js
+++ cypress/integration/Learner/pupychupy.js
@@ -2,9 +2,23 @@
 describe('practice', ( )=> {
     it('practicing cypress', () =>{
 
-        cy.visit('https://demo.automationtesting.in/');
-       // cy.get('input[class="gLFyf gsfi"]').type('sports');
-       // cy.get(':nth-child(1) > .eIPGRd > .pcTkSc > .wM6W7d > span').click();
+       //cy.visit('https://the-internet.herokuapp.com/');
+       //cy.wait(5000);
+       //cy.get(':nth-child(1) > a').should("be.visible").click();
+       //cy.wait(5000);
+       //cy.go('back');
+       //cy.wait(5000);
+       //cy.get('a[href="/add_remove_elements/"]').should("be.visible").click();
+       //cy.wait(5000);
+       //cy.go('back');
+
+       cy.visit ('https://www.techbeamers.com/websites-to-practice-selenium-webdriver-online/');
+       cy.get("img[class='aligncenter']").click();
+       cy.wait(1000);
+       cy.get("a[rel='noopener']").click();
+
+
+
 
     })
 
