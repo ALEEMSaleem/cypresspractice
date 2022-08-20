8d9c7f7be0481640e004894e3f8b17126426c99f
muhammad saleem <sarowerhome@gmail.com>
2022-08-19 22:56:44 -0400

    new code

--- cypress/integration/Learner/pupychupy.js
+++ cypress/integration/Learner/pupychupy.js
@@ -12,10 +12,16 @@ describe('practice', ( )=> {
        //cy.wait(5000);
        //cy.go('back');
 
-       cy.visit ('https://www.techbeamers.com/websites-to-practice-selenium-webdriver-online/');
-       cy.get("img[class='aligncenter']").click();
-       cy.wait(1000);
-       cy.get("a[rel='noopener']").click();
+       //cy.visit ('https://www.techbeamers.com/websites-to-practice-selenium-webdriver-online/');
+       //cy.get("img[class='aligncenter']").click();
+       //cy.wait(1000);
+       //cy.get("a[rel='noopener']").click();
+
+       cy.visit("http://demo.automationtesting.in/");
+       cy.get("#btn1").should("be.visible").click();
+       cy.get("input[placeholder='E mail']").type('zakiraleem@yahoo.com');
+       cy.get("input[placeholder='Password']").type('hero#1');
+       cy.get("#enterbtn").click();
 
 
 
